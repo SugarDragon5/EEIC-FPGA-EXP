7645bfedb3f43cd0137771509774de3b08eb0f1a
Ryugo SATO <sugardragon2nd@gmail.com>
2023-10-17 15:34:17 +0900

    feature: 2点交換ロジックを実装。

---
+++ swap.sv
@@ -0,0 +1,90 @@
+`include "distance.v"
+module checkswap (
+    clk,rst,
+    x1,y1,
+    x2,y2,
+    x3,y3,
+    x4,y4,
+    x5,y5,
+    x6,y6,
+    res,
+    complete
+);
+    input clk;
+    input rst;
+    input [31:0] x1,y1,x2,y2,x3,y3,x4,y4,x5,y5,x6,y6;
+    output reg res;
+    output reg complete;
+    reg [10:0] cnt;
+    reg [31:0] inx1,iny1,inx2,iny2;
+    wire [31:0] out;
+    reg [31:0] sum1,sum2;
+    distance calc(.x1(inx1),.y1(iny1),.x2(inx2),.y2(iny2),.res(out));
+    always @(posedge clk) begin
+        if(rst)begin
+            res<=0;
+            complete<=0;
+            cnt<=0;
+            sum1<=0;
+            sum2<=0;
+        end
+        else if(complete==0)begin
+            if(cnt==0)begin
+                inx1<=x1;
+                iny1<=y1;
+                inx2<=x2;
+                iny2<=y2;
+            end else if(cnt==1)begin
+                inx1<=x2;
+                iny1<=y2;
+                inx2<=x3;
+                iny2<=y3;
+                sum1<=sum1+out; //(v1,v2)
+            end else if(cnt==2)begin
+                inx1<=x4;
+                iny1<=y4;
+                inx2<=x5;
+                iny2<=y5;
+                sum1<=sum1+out; //(v2,v3)
+            end else if(cnt==3)begin
+                inx1<=x5;
+                iny1<=y5;
+                inx2<=x6;
+                iny2<=y6;
+                sum1<=sum1+out; //(v4,v5)
+            end else if(cnt==4)begin
+                inx1<=x1;
+                iny1<=y1;
+                inx2<=x5;
+                iny2<=y5;
+                sum1<=sum1+out; //(v5,v6)
+            end else if(cnt==5)begin
+                inx1<=x5;
+                iny1<=y5;
+                inx2<=x3;
+                iny2<=y3;
+                sum2<=out; //(v1,v5)
+            end else if(cnt==6)begin
+                inx1<=x4;
+                iny1<=y4;
+                inx2<=x2;
+                iny2<=y2;
+                sum2<=sum2+out; //(v5,v3)
+            end else if(cnt==7)begin
+                inx1<=x2;
+                iny1<=y2;
+                inx2<=x6;
+                iny2<=y6;
+                sum2<=sum2+out; //(v4,v2)
+            end else if(cnt==8)begin
+                sum2<=sum2+out; //(v2,v6)
+            end else if(cnt==9)begin
+                if(sum1>sum2)res<=1;
+                else res<=0;
+                complete<=1;
+            end
+            cnt<=cnt+1;
+        end
+    end
+
+endmodule
---
+++ test_swap.sv
@@ -0,0 +1,58 @@
+`include "swap.sv"
+`default_nettype none
+
+module tb_swap;
+reg clk;
+reg rst_n;
+reg [31:0] xs[5:0];
+reg [31:0] ys[5:0];
+wire res;
+wire complete;
+checkswap checkswap
+(
+    .rst (rst_n),.clk (clk),
+    .x1(xs[0]), .y1(ys[0]),
+    .x2(xs[1]), .y2(ys[1]),
+    .x3(xs[2]), .y3(ys[2]),
+    .x4(xs[3]), .y4(ys[3]),
+    .x5(xs[4]), .y5(ys[4]),
+    .x6(xs[5]), .y6(ys[5]),
+    .res(res), .complete(complete)
+);
+
+localparam CLK_PERIOD = 10;
+always #(CLK_PERIOD/2) clk=~clk;
+
+initial begin
+    $dumpfile("tb_swap.vcd");
+    $dumpvars(0, tb_swap);
+end
+
+initial begin
+    #1 rst_n=1'bx;clk=1'b0;
+    #(CLK_PERIOD*3)
+        rst_n=1;
+        xs[0]=32'd00000000;ys[0]=32'd00000000;
+        xs[1]=32'd00000010;ys[1]=32'd00000000;
+        xs[2]=32'd00000030;ys[2]=32'd00000000;
+        xs[3]=32'd00000060;ys[3]=32'd00000000;
+        xs[4]=32'd00000100;ys[4]=32'd00000000;
+        xs[5]=32'd00000150;ys[5]=32'd00000000;
+
+    #(CLK_PERIOD*3) rst_n=0;
+    repeat(15) @(posedge clk);
+    #(CLK_PERIOD*3)
+        rst_n=1;
+        xs[0]=32'd00000000;ys[0]=32'd00000000;
+        xs[1]=32'd00000100;ys[1]=32'd00000000;
+        xs[2]=32'd00000030;ys[2]=32'd00000000;
+        xs[3]=32'd00000060;ys[3]=32'd00000000;
+        xs[4]=32'd00000010;ys[4]=32'd00000000;
+        xs[5]=32'd00000150;ys[5]=32'd00000000;
+    #(CLK_PERIOD*3) rst_n=0;
+    repeat(15) @(posedge clk);
+    $finish(2);
+end
+
+endmodule
+`default_nettype wire
