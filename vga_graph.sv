cc85f1d5d727591dd0fb1ec8320139061451ee03
Ryugo SATO <sugardragon2nd@gmail.com>
2023-10-27 02:19:44 +0900

    fix: 条件式のエラーを修正。

--- vga_graph.sv
+++ vga_graph.sv
@@ -8,7 +8,7 @@ module VGAGraph (
     reg cnt[15:0];
     wire [7:0] X,Y;
     assign {X,Y}=cnt;
-    integer [5:0] i;
+    integer i;
     always @(posedge clk) begin
         if(rst)begin
             cnt<=0;
@@ -26,8 +26,8 @@ module VGAGraph (
                 if(xs[path[i]]<=X&&X<=xs[path[i+1]]||xs[path[i+1]]<=X&&X<=xs[path[i]])begin
                     if(ys[path[i]]<=Y&&Y<=ys[path[i+1]]||ys[path[i+1]]<=Y&&Y<=ys[path[i]])begin
                         if(
-                            ((ys[path[i+1]]-ys[path[i]])*(X-xs[path[i]])-(xs[path[i+1]]-xs[path[i]])*(Y-ys[path[i]]))*((ys[path[i+1]]-ys[path[i]])*(i+1-xs[path[i]])-(xs[path[i+1]]-xs[path[i]])*(Y+1-ys[path[i]]))<=0 
-                            || ((ys[path[i+1]]-ys[path[i]])*(i+1-xs[path[i]])-(xs[path[i+1]]-xs[path[i]])*(Y-ys[path[i]]))*((ys[path[i+1]]-ys[path[i]])*(i-xs[path[i]])-(xs[path[i+1]]-xs[path[i]])*(Y+1-ys[path[i]]))<=0
+                            ((ys[path[i+1]]-ys[path[i]])*(X-xs[path[i]])-(xs[path[i+1]]-xs[path[i]])*(Y-ys[path[i]]))*((ys[path[i+1]]-ys[path[i]])*(X+1-xs[path[i]])-(xs[path[i+1]]-xs[path[i]])*(Y+1-ys[path[i]]))<=0 
+                            || ((ys[path[i+1]]-ys[path[i]])*(X+1-xs[path[i]])-(xs[path[i+1]]-xs[path[i]])*(Y-ys[path[i]]))*((ys[path[i+1]]-ys[path[i]])*(X-xs[path[i]])-(xs[path[i+1]]-xs[path[i]])*(Y+1-ys[path[i]]))<=0
                         )begin
                             R[X][Y]<=0;
                             G[X][Y]<=0;
